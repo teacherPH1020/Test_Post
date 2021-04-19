a9ef2550a40efda061cd5e509175bfa84edf9a47
Sergey Po <postnikov_s@teacher.itstep.by>
2021-04-19 21:05:10 +0300

    script added
    first module to get name

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 Test project ot learch Git and using GitHub.
 
-1. Changed for testing Fetch. New.
+1. Changed for testing Fetch.
+2. Changed local on my Desktop.
 
 
 Sergey P.
---
+++ new.py
@@ -0,0 +1,9 @@
+print("hello GitHub")
+class MyName():
+    def __init__(self):
+        self.my_name = None
+    
+    def __call__(self, name = 'Incoginto'):
+        self.my_name = name
+        return "name memorized"
+    
